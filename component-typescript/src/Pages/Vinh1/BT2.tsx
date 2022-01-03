573f77a6ba5b02a877f101ada2e5a1c26cb00386
LoiVo <vothanhloi.29@gmail.com>
2022-01-03 22:36:01 +0700

    [upd] BT2

--- component-typescript/src/Pages/Vinh1/BT2.tsx
+++ component-typescript/src/Pages/Vinh1/BT2.tsx
@@ -12,6 +12,19 @@ interface IBodyLeft {
     styleName: string
     colorName: string,
 }
+export interface ITable {
+    ocNumber:      string;
+    sizeCode:      string;
+    sizeName:      string;
+    colorCode:     string;
+    colorName:     string;
+    styleName:     string;
+    styleCode:     string;
+    orderQty:      number;
+    offeredQty:    number;
+    inspectionQty: number;
+    acceptedQty:   number;
+}
 
 const BT2 = () => {
 
@@ -49,6 +62,62 @@ const BT2 = () => {
     }
 
 
+    const total = (listArray: any) => {
+        listArray.map((item: any) => item.orderQty).reduce(((previousValue: any, currentValue: any) => previousValue + currentValue?.orderQty),0)
+        
+    }
+
+    const getSum = (array: any, column: string) => {
+     let values = array.map((item: any) => parseInt(item[column]) || 0)
+        return values.reduce((a: any, b: any) => a + b)
+    }
+
+    var groupBy = function(xs: any, key: any) {
+        return xs.reduce(function(rv: any, x: any) {
+          (rv[x[key]] = rv[x[key]] || []).push(x);
+          return rv;
+        }, {});
+      };
+      console.log(groupBy(listArray, 'sizeName'));
+   // const groupBy = (x: any,f: any)=>x.reduce((a: any,b: any)=>((a[f(b)]||=[]).push(b),a),{});
+
+    
+    var people = [
+        { name: 'Alice', age: 21 },
+        { name: 'Max', age: 20 },
+        { name: 'Jane', age: 20 }
+      ];
+      
+      const  groupBy2 = (objectArray: any, property: any) =>{
+        return objectArray.reduce(function (acc: any, obj: any) {
+          var key = obj[property];
+          if (!acc[key]) {
+            acc[key] = [];
+          }
+          acc[key].push(obj);
+          return acc;
+        }, {});
+      }
+      
+      var groupedSizeName = groupBy2(listArray, 'sizeName');
+      console.log("groupedPeople",groupedSizeName);
+      
+
+
+
+    console.log("groupBy", groupBy(listArray, (v: any)=> v.sizeName));
+
+    console.log("tong orderQty", getSum(listArray, 'orderQty'));
+    console.log("tong offeredQty", getSum(listArray, 'offeredQty'));
+    console.log("tong inspectionQty", getSum(listArray, 'inspectionQty'));
+
+
+    console.log("bodyLeft", bodyLeft);
+    console.log("listArray", listArray);
+    console.log("header", header);
+    
+
+    
     return (
         <div className='BT2 py-2' style={{overflow: 'auto', width:'1300px',maxWidth:'1300px'}}>
             <table>
@@ -93,10 +162,16 @@ const BT2 = () => {
                             } )
                             }
                         </tr>
+                       
                     </React.Fragment>
                     ))
                     }     
                 </tbody>
+                <tfoot>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tfoot>
             </table>
         </div>
     )
