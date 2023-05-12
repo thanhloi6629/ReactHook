76acddef5eb9701ae15b8337b5f284e46fd5b5d0
LoiVo <vothanhloi.29@gmail.com>
2023-05-12 18:55:50 +0700

    [upd] web3D

---
+++ web3D/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      3D developer Portfolio
+    </>
+  )
+}
+
+export default App
---
+++ web3D/src/components/About.jsx
@@ -0,0 +1,12 @@
+
+const About = () => {
+  return (
+    <>
+      <div>about</div>
+
+     
+    </>
+  );
+};
+
+export default About
